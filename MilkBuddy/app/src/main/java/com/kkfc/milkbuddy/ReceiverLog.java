cc99c846943c058e0d1e2e1604ee5972f364b2f1
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-01-16 22:32:20 -0500

    Button lead to pages

---
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ReceiverLog.java
@@ -0,0 +1,14 @@
+package com.kkfc.milkbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiverLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver_log);
+    }
+}
