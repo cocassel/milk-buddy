c96ea35cd484881fe724e912648ce9c443ac7b8a
Celeste Cassel de Camps <celestec@rogers.com>
2020-01-29 16:56:05 -0500

    Added farmers and receivers import functionality

--- MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/MainActivity.java
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/MainActivity.java
@@ -1,17 +1,24 @@
 package com.kkfc.milkbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper db;
     private Button start;
     private Button start2;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +30,17 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+                    //Toast.makeText(MainActivity.this, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);                }
                 importNewData();
             }
         });
+
         start2 = findViewById(R.id.button2);
         start2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +52,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void importNewData() {
+
+        //db.verifyStoragePermissions(this);
         db.insertTransportersFromCSV();
+        db.insertReceiversFromCSV();
+        db.insertFarmersFromCSV();
+
         Intent intent = new Intent(this, Start.class);
         startActivity(intent);
     }
