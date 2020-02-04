aac644c1af702a469c2faf9c712e467bd7edf090
Malvika <malvi97@gmail.com>
2020-02-04 16:01:02 -0500

    Transporter Homepage UI

---
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/TransporterHomepage.java
@@ -0,0 +1,24 @@
+package com.kkfc.milkbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class TransporterHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transporter_homepage);
+
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.Spinner1);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(TransporterHomepage.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.routeDropdown));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+    }
+}
