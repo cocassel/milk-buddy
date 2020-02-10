71a5fc2ba1445c2cc73dac17feb8564ac8063870
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-02-10 16:29:42 -0500

    SMALL details

---
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/FarmerCollection.java
@@ -0,0 +1,14 @@
+package com.kkfc.milkbuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FarmerCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_collection);
+    }
+}
