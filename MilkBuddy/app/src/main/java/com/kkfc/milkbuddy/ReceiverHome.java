a7ecb26185bc3e8d114c30ac1282b6ecf3557b97
Malvika <malvi97@gmail.com>
2020-02-25 15:44:51 -0500

    Update ReceiverHome.java

--- MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ReceiverHome.java
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ReceiverHome.java
@@ -19,11 +19,6 @@ public class ReceiverHome extends AppCompatActivity {
     private ListView containerListView;
     SimpleCursorAdapter containerCursorAdapter;
 
-    // THE DESIRED COLUMNS TO BE BOUND
-   // final String[] containerColumns = new String[]{ db.CONTAINER_ID };
-
-    // THE XML DEFINED VIEWS WHICH THE DATA WILL BE BOUND TO
-    //final int[] containerTo = new int[]{ R.id.CONTAINER_ID};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,40 +36,9 @@ public class ReceiverHome extends AppCompatActivity {
         String loggedInTransporter = cursor.getString(cursor.getColumnIndex(db.TRANSPORTER_NAME));
         Log.i("logged in", loggedInTransporter);
         NAME= (TextView)findViewById(R.id.transporterName);
-        NAME.setText("These are  " + loggedInTransporter + "'s containers");
-        
+        NAME.setText("These are " + loggedInTransporter + "'s containers");
 
-
-       /* // List containers
-        Cursor containerCursor = db.fetchContainers();
-        containerListView = findViewById(R.id.list_view);
-
-        if (containerCursor.getCount() == 0) {
-            Toast.makeText(this, "No data to show", Toast.LENGTH_LONG).show();
-        } else {
-            containerCursorAdapter = new SimpleCursorAdapter(this, R.layout.transporter_list_entry, containerCursor, containerColumns, containerTo, 0);
-            containerListView.setAdapter(containerCursorAdapter);
-        }*/
     }
-
-
-
-        /*transporterListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                goToTransporterHomepage();
-
-                // When a transporter is selected, fetch the ID of the selected transporter
-                Cursor cursor = ((SimpleCursorAdapter)parent.getAdapter()).getCursor();
-                cursor.moveToPosition(position);
-                int selectedTransporterId = cursor.getInt(cursor.getColumnIndex(db.TRANSPORTER_ID));
-                String selectedTransporterName = cursor.getString(cursor.getColumnIndex(db.TRANSPORTER_NAME));
-                String selectedTransporterPhoneNumber = cursor.getString(cursor.getColumnIndex(db.TRANSPORTER_PHONE_NUMBER));
-
-                // Save logged-in transporter to database
-                db.insertLoggedInTransporter(selectedTransporterId, selectedTransporterName, selectedTransporterPhoneNumber);
-            }
-        });*/
 
 
 
