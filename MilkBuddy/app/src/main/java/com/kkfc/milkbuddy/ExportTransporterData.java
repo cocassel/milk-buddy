b2c774c480a6d8b3fa8e4da5faf81934abce4c92
Celeste Cassel de Camps <celestec@rogers.com>
2020-03-04 21:39:27 -0500

    Write actual transporter data to csv

--- MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ExportTransporterData.java
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ExportTransporterData.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -67,18 +68,24 @@ public class ExportTransporterData extends AppCompatActivity {
                     if(cursor.getCount() == 0) {
                         Toast.makeText(this, "No data to export", Toast.LENGTH_LONG).show();
                     } else {
+                        // Write column names to file as a header
+                        String[] columnNamesArray = cursor.getColumnNames();
+                        String columnNamesString = TextUtils.join(",", columnNamesArray);
+                        writer.write(columnNamesString + "\n");
+
+                        // Write actual data to file
                         while(cursor.moveToNext()) {
-                            // 1 is the column for first name
-                            String blah = cursor.getString(1) + " " + cursor.getString(2);
-                            // TODO
-                            // write header
 
-                            writer.write("DO I WORK?, YES\n");
+                            String[] valuesArray = new String[cursor.getColumnCount()];
 
+                            for(int i=0; i < cursor.getColumnCount(); i ++ ) {
+                                valuesArray[i] = cursor.getString(i);
+                            }
+
+                            String valuesString = TextUtils.join(",", valuesArray);
+                            writer.write(valuesString + "\n");
                         }
                     }
-
-
                     
                     writer.close();
 
