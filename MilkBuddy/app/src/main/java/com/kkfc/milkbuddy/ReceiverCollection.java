338d8b86a2cbeb02aede7385787d75369903b31a
a2dakhqa <43449620+a2dakhqa@users.noreply.github.com>
2020-03-05 22:31:05 -0500

    Start

--- MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ReceiverCollection.java
+++ MilkBuddy/app/src/main/java/com/kkfc/milkbuddy/ReceiverCollection.java
@@ -1,20 +1,40 @@
 package com.kkfc.milkbuddy;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ReceiverCollection extends AppCompatActivity {
 
+    DatabaseHelper db;
     AlertDialog.Builder builder;
     private Button cancelCollection;
+    private Button saveCollection;
+    private Button transporterTextView;
+    private TextView containerTextView;
+    private EditText quantity;
+    private EditText quantityEmpty;
+    private int transporterId;
+    private int containerId;
+    private int receiverId;
+    private RadioButton sniffPass, sniffFail, sniffNa, alcoholPass, alcoholFail, alcoholNa,densityTwoSeven, densityTwoEight, densityTwoNine, densityThirty, densityThirtyOnePlus, densityNa ;
+    private String sniffTest;
+    private String alcoholTest;
+    private String densityTest;
+    private String dateToday;
+    private String timeToday;
 
 
 
@@ -22,6 +42,18 @@ public class ReceiverCollection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver_collection);
+
+        db = new DatabaseHelper(this);
+        //Fetch the loggedInTransporter to show on the UI
+        Cursor cursor = db.fetchLoggedInTransporter();
+        //only one row in the table so use first row
+        cursor.moveToFirst();
+        transporterId = cursor.getInt(cursor.getColumnIndex(db.TRANSPORTER_ID));
+        String loggedInTransporter = cursor.getString(cursor.getColumnIndex(db.TRANSPORTER_NAME));
+        transporterTextView = findViewById(R.id.textView1);
+        transporterTextView.setText("Transporter: " );
+
+
 
         // Cancel Collection Process
         cancelCollection = findViewById(R.id.Button01);
@@ -33,6 +65,12 @@ public class ReceiverCollection extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
+        // Save Collection Process
+
+
+
     }
 
     private void returnToReceiverHome() {
